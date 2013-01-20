9e4edde91c21f6ab5f81276207cbe9fb4955c762
Renato Athaydes <renatoathaydes@hotmail.com>
2013-01-20 21:59:08 +0100

    Fixed some OSGi configuration problems and added new TestOsgiApp to try and start a bundle

---
+++ test-osgi-app/src/main/groovy/com/smartbear/edp/testosgiapp/TestOsgiApp.groovy
@@ -0,0 +1,31 @@
+package com.smartbear.edp.testosgiapp
+
+import com.smartbear.edp.api.EventManager
+
+/**
+ * A simple example on setting up and starting a Spring-OSGi app.
+ * Based on this:
+ * http://java.dzone.com/news/osgi-and-spring-dynamic
+ * User: Renato
+ */
+class TestOsgiApp {
+
+	TestOsgiApp( EventManager manager ) {
+		stars()
+		println "Got the EventManager: ${manager.class.name}"
+		stars()
+	}
+
+	def stars() { println '*' * 50 }
+
+	void start() {
+		stars()
+		println 'Starting TestOsgiApp'
+	}
+
+	void stop() {
+		stars()
+		println 'Stopping TestOsgiApp'
+	}
+
+}
